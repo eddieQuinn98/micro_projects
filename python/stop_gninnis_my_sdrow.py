7bf96b124dbe5511ed3fa9d68630066b823bf518
eddiebquinn <github@pranonym.com>
2022-10-02 01:48:56 +0100

    refactor: Refactored stop_gninnis_my_sdrows code

--- python/stop_gninnis_my_sdrow.py
+++ python/stop_gninnis_my_sdrow.py
@@ -3,6 +3,7 @@ Original codewars challenge: https://www.codewars.com/kata/5264d2b162488dc400000
 Date: 18-06-22
 """
 
+# ORIGNAL VERSION
 
 def flip_word(word: str):
     new_word = ""
@@ -11,7 +12,7 @@ def flip_word(word: str):
     return new_word
 
 
-def spin_words(sentence: str, word_length: int = 5):
+def spin_words_depreciated(sentence: str, word_length: int = 5):
     new_sentence = ""
     sentence_as_list = sentence.split()
     position = 0
@@ -21,6 +22,11 @@ def spin_words(sentence: str, word_length: int = 5):
             spacing if len(word) > (word_length-1) else word + spacing
         position = position + 1
     return new_sentence
+
+
+# NEW VERSION (02-10-2022)
+def spin_words(s:str, ln:int=5):
+     return " ".join((w if len(w) < ln else "".join(l for l in w[::-1])) for w in s.split())
 
 
 if __name__ == "__main__":
