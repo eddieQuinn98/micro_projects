1ebabb47c02b6b4587ab55713fb4c52dc447a5fc
eddiebquinn <github@pranonym.com>
2022-10-06 01:45:27 +0100

    feat(JavaScript): Added OddEven JS KATA

---
+++ javascript/CodeWars/8 - evenOrOdd.js
@@ -0,0 +1,9 @@
+// Original KATA: https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+// Date: 06/10/22
+//
+// This is my first JS KATA
+
+
+function even_or_odd(number) {
+  return number%2 === 0 ? "Even" : "Odd";
+}
