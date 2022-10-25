5c196abcedc1bcb0f535e60f3ad8e57dff5ebb77
eddiebquinn <github@pranonym.com>
2022-10-25 01:07:10 +0100

    refactor(Python):

--- 1.Codewars/5 - beeramin.py
+++ 1.Codewars/5 - beeramin.py
@@ -9,12 +9,3 @@ def beeramid(bonus:int, price:int):
 
 if __name__ == "__main__":
     print(beeramid(21, 1.5))
-    """print(beeramid(10, 2))
-    print(beeramid(11, 2))
-    print(beeramid(21, 1.5))
-    print(beeramid(454, 5))
-    print(beeramid(455, 5))
-    print(beeramid(4, 4))
-    print(beeramid(3, 4))
-    print(beeramid(0, 4))
-    print(beeramid(-1, 4))"""
