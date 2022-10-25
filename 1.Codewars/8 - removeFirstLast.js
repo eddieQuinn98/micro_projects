11cd421f6d0cc93ad318045a25565aa001bc0f5b
eddiebquinn <github@pranonym.com>
2022-10-25 03:42:45 +0100

    feat(JavaScript): Added code for JS remove first/last

---
+++ 1.Codewars/8 - removeFirstLast.js
@@ -0,0 +1,8 @@
+// KATA Link: https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/solutions/javascript
+// Date: 25/10/22
+
+function removeChar(str){
+    return str.slice(1, -1);
+   }
+
+console.log(removeChar("hello"));
